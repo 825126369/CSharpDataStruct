89e97179db6e6f46b00041af1c0736f696d876fd
xuke825126369 <xuke825126369@gmail.com>
2021-09-26 23:48:18 +0800

    直接插入排序，希尔插入排序，快速排序，冒泡排序

--- xkDic/Sort/Program.cs
+++ xkDic/Sort/Program.cs
@@ -7,19 +7,23 @@ namespace Sort
     {
         static void Main(string[] args)
         {
+            int nArrayCount = 10;
             List<int> sortList = new List<int>();
             Random mRandom = new Random();
-            for(int i = 0; i <= 100; i++)
+            for(int i = 0; i <= nArrayCount; i++)
             {
-                sortList.Add(mRandom.Next(0, 100));
+                sortList.Add(mRandom.Next(0, nArrayCount));
+                Console.Write("" + sortList[i] + ",");
             }
+
+            Console.WriteLine();
 
             QuickSort.Sort(sortList);
 
             for (int i = 0; i < sortList.Count; i++)
             {
                 Console.Write("" + sortList[i] + ",");
-                if (i % 20 == 0)
+                if (i % 20 == 0 && i > 0)
                 {
                     Console.WriteLine();
                 }
--- xkDic/Sort/QuickSort.cs
+++ xkDic/Sort/QuickSort.cs
@@ -6,23 +6,126 @@ namespace Sort
 {
     public class QuickSort
     {
-        //冒泡排序
-        //1: 每次确定一个最大值，
-        //2:记录相邻元素移动次数，如果为0表示移动完成
+        //快速排序
+        //1: 随机找到列表中索引临界值，使得列表中左边的数小于等于临界值，右边的数大于等于临界值
+        //2；递归
         public static void Sort(List<int> sortList)
         {
-            Sort(sortList, 0, sortList.Count - 1);
+            Sort2(sortList, 0, sortList.Count - 1);
+        }
+
+        public static void Sort2(List<int> sortList, int nBeginIndex, int nEndIndex)
+        {
+            if (nBeginIndex < nEndIndex)
+            {
+                int nCompareIndex = nBeginIndex + (nEndIndex - nBeginIndex) / 2;
+                int nCompareValue = sortList[nCompareIndex];
+
+                int nIndex1 = nBeginIndex;
+                int nIndex2 = nEndIndex;
+
+                while (nIndex1 < nIndex2)
+                {
+                    while (nIndex2 > nCompareIndex && sortList[nIndex2] >= nCompareValue)
+                    {
+                        nIndex2--;
+                    }
+
+                    while (nIndex1 < nCompareIndex && sortList[nIndex1] <= nCompareValue)
+                    {
+                        nIndex1++;
+                    }
+
+                    for (int i = nIndex1; i <= nIndex2; i++)
+                    {
+                        if (i > nCompareIndex && sortList[i] < nCompareValue)
+                        {
+                            sortList[nCompareIndex] = sortList[i];
+                            sortList[i] = nCompareValue;
+                            nCompareIndex = i;
+                        }
+                    }
+
+                    for (int i = nIndex1; i <= nIndex2; i++)
+                    {
+                        if (i < nCompareIndex && sortList[i] > sortList[nCompareIndex])
+                        {
+                            sortList[nCompareIndex] = sortList[i];
+                            sortList[i] = nCompareValue;
+                            nCompareIndex = i;
+                        }
+                    }
+                }
+
+                for (int i = nBeginIndex; i <= nEndIndex; i++)
+                {
+                    if (i < nCompareIndex)
+                    {
+                        Debug.Assert(sortList[nCompareIndex] >= sortList[i], sortList[nCompareIndex] + " | " + sortList[i]);
+                    }
+                    else if (i > nCompareIndex)
+                    {
+                        Debug.Assert(sortList[nCompareIndex] <= sortList[i], sortList[nCompareIndex] + " | " + sortList[i]);
+                    }
+                }
+
+                Sort2(sortList, nBeginIndex, nCompareIndex - 1);
+                Sort2(sortList, nCompareIndex + 1, nEndIndex);
+            }
+        }
+
+        public static void Sort1(List<int> sortList, int nBeginIndex, int nEndIndex)
+        {
+            if (nBeginIndex < nEndIndex)
+            {
+                int nCompareIndex = nBeginIndex;
+                int nCompareValue = sortList[nCompareIndex];
+                
+                int nIndex1 = nBeginIndex;
+                int nIndex2 = nEndIndex;
+
+                while (nIndex1 < nIndex2)
+                {
+                    while (sortList[nIndex2] >= nCompareValue && nIndex2 > nIndex1)
+                    {
+                        nIndex2--;
+                    }
+                    
+                    sortList[nIndex1] = sortList[nIndex2];
+
+                    while (sortList[nIndex1] <= nCompareValue && nIndex2 > nIndex1)
+                    {
+                        nIndex1++;
+                    }
+                    
+                    sortList[nIndex2] = sortList[nIndex1];
+                }
+
+                sortList[nIndex1] = nCompareValue;
+                nCompareIndex = nIndex2;
+
+                for (int i = nBeginIndex; i <= nEndIndex; i++)
+                {
+                    if (i < nCompareIndex)
+                    {
+                        Debug.Assert(sortList[nCompareIndex] >= sortList[i], sortList[nCompareIndex] + " | " + sortList[i]);
+                    }
+                    else if (i > nCompareIndex)
+                    {
+                        Debug.Assert(sortList[nCompareIndex] <= sortList[i], sortList[nCompareIndex] + " | " + sortList[i]);
+                    }
+                }
+
+                Sort1(sortList, nBeginIndex, nCompareIndex - 1);
+                Sort1(sortList, nCompareIndex + 1, nEndIndex);
+            }
         }
 
         public static void Sort(List<int> sortList, int nBeginIndex, int nEndIndex)
         {
-            if(nBeginIndex >= nEndIndex)
+            if (nBeginIndex < nEndIndex)
             {
-
-            }
-            else
-            {
-                int nCompareIndex = nBeginIndex;
+                int nCompareIndex = nBeginIndex + (nEndIndex - nBeginIndex) / 2;
                 while (true)
                 {
                     int nSwitchCount = 0;
@@ -52,7 +155,7 @@ namespace Sort
                         }
                     }
 
-                    if(nSwitchCount == 0)
+                    if (nSwitchCount == 0)
                     {
                         break;
                     }

50ccdb3f853db57d2f7363fed9e20f2a59504693
xuke825126369 <xuke825126369@gmail.com>
2021-10-18 18:50:51 +0800

    2

---
+++ xkDic/DigetLink/归档/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DigetLink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random mRandom = new Random();
+            for (int i = 0; i < 100000; i++)
+            {
+                uint Input1 = (uint)mRandom.Next();
+                uint Input2 = (uint)mRandom.Next();
+
+                //uint Input1 = uint.MaxValue;
+                //uint Input2 = 2;
+
+                uint result1 = IntegerAdd(Input1, Input2);
+                Console.WriteLine("result1: " + result1);
+            }
+        }
+
+        static uint IntegerAdd(uint A, uint B)
+        {
+            IntegerLinkedList result = new IntegerLinkedList(A) + new IntegerLinkedList(B);
+            uint result1 = result.ToInteger();
+            Debug.Assert(result1 == A + B, result1 + " | " + A + " + " + B + " | " + (A + B));
+            return result1;
+        }
+
+    }
+}
